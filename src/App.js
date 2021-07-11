dd3dc8e95d7dfaac5c5987f21ef36b5106afae71
ggneiracc68 <ggneiracc68@gmail.com>
2021-07-10 22:35:00 -0500

    Version 3 de React con 8 componentes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import C01componente from './components/C01componente';
 import C02contador from './components/C02contador';
 import C03componenteConCss from './components/C03CompCss';
 import EjemploDeEstado from './components/C04estado';
+import EjemploDeDobleEstado from './components/C05dobleestado';
+import ComponenteConVariable from './components/C06variable';
+import OperadorTernario from './components/C07operador';
+import Matriz from './components/C08matriz';
 
 function App() {
   return ( 
@@ -16,7 +20,14 @@ function App() {
       <C03componenteConCss />
       <hr/>
       <EjemploDeEstado />
-      
+      <hr/>
+      <EjemploDeDobleEstado />
+      <hr/>
+      <ComponenteConVariable xVariable="Hola mundo desde... props..." />
+      <hr/>
+      <OperadorTernario xEdad = '20'/>
+      <hr/>
+      <Matriz />
     </div>
   );
 }
